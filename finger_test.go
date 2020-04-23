21089b1cbc65d78081fe15c83017f422fb01bda1
latashalenus <latashalenus@users.noreply.github.com>
2020-04-23 11:59:13 +0800

    added more tests

--- finger_test.go
+++ finger_test.go
@@ -24,17 +24,34 @@ func Test_NewFingerTable(t *testing.T) {
 		// TODO: Add test cases.
 		// {"1", args{NewInode("8", "0.0.0.0:8083"), 1}, fingerTable},
                 {
-                        "standard", args{NewInode("1", "0.0.0.0:8001"), 2}, []*fingerEntry{
-                                &fingerEntry{
-                                        Id: fingerID(NewInode("1", "0.0.0.0:8001").Id, 0, 2),
-                                        Node: NewInode("1", "0.0.0.0:8001"),
-                                },
-                                &fingerEntry{
-                                        Id: fingerID(NewInode("1", "0.0.0.0:8001").Id, 1, 2),
-                                        Node: NewInode("1", "0.0.0.0:8001"),
-                                },
-                        },
-                },
+					"standard", args{NewInode("1", "0.0.0.0:8001"), 2}, []*fingerEntry{
+							&fingerEntry{
+									Id: fingerID(NewInode("1", "0.0.0.0:8001").Id, 0, 2),
+									Node: NewInode("1", "0.0.0.0:8001"),
+							},
+							&fingerEntry{
+									Id: fingerID(NewInode("1", "0.0.0.0:8001").Id, 1, 2),
+									Node: NewInode("1", "0.0.0.0:8001"),
+							},
+					},
+				},
+				
+				{
+					"standard3", args{NewInode("1", "0.0.0.0:8002"), 3}, []*fingerEntry{
+							&fingerEntry{
+									Id: fingerID(NewInode("1", "0.0.0.0:8002").Id, 0, 3),
+									Node: NewInode("1", "0.0.0.0:8002"),
+							},
+							&fingerEntry{
+									Id: fingerID(NewInode("1", "0.0.0.0:8002").Id, 1, 3),
+									Node: NewInode("1", "0.0.0.0:8002"),
+							},
+							&fingerEntry{
+								Id: fingerID(NewInode("1", "0.0.0.0:8002").Id, 2, 3),
+								Node: NewInode("1", "0.0.0.0:8002"),
+							},
+					},
+				},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -57,10 +74,22 @@ func Test_NewFingerEntry(t *testing.T) {
 	}{
 		// TODO: Add test cases.
                 {
-                        "standard", 
-                        args{[]byte{0,0,0}, NewInode("1", "0.0.0.0:8001")}, 
-                        &fingerEntry{Id:[]byte{0,0,0}, Node:NewInode("1", "0.0.0.0:8001")},
-                },
+					"standard", 
+					args{[]byte{0,0,0}, NewInode("1", "0.0.0.0:8001")}, 
+					&fingerEntry{Id:[]byte{0,0,0}, Node:NewInode("1", "0.0.0.0:8001")},
+				},
+				
+				{
+					"standard1", 
+					args{[]byte{0,0,1}, NewInode("1", "0.0.0.0:8001")}, 
+					&fingerEntry{Id:[]byte{0,0,1}, Node:NewInode("1", "0.0.0.0:8001")},
+				},
+				
+				{
+					"standard2", 
+					args{[]byte{0,0,1}, NewInode("1", "0.0.0.0:8002")}, 
+					&fingerEntry{Id:[]byte{0,0,1}, Node:NewInode("1", "0.0.0.0:8002")},
+				},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -83,11 +112,23 @@ func Test_FingerID(t *testing.T) {
 		want []byte
 	}{
 		// TODO: Add test cases.
-                {
-                        "standard",
-                        args{[]byte{1}, 2, 3},
-                        []byte{5},
-                },
+			{
+					"standard",
+					args{[]byte{1}, 2, 3},
+					[]byte{5},
+			},
+			
+			{
+				"standard1",
+				args{[]byte{2}, 2, 3},
+				[]byte{6},
+			},
+
+			{
+				"standard1",
+				args{[]byte{1,1}, 2, 3},
+				[]byte{5},
+			},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
