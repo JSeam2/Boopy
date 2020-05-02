2b643e91359f28f897ad7bbf31d459f24136d114
angsty-duck <solsticedante@gmail.com>
2020-05-02 16:38:31 +0800

    Refactored code - Increase clarity and removed unnecessary code

--- finger.go
+++ finger.go
@@ -1,35 +1,34 @@
 package boopy
 
 import (
+	"github.com/jseam2/boopy/api"
 	"fmt"
 	"math/big"
-
-	"github.com/jseam2/boopy/api"
 )
 
 type fingerTable []*fingerEntry
 
 // Generate new finger table, with empty entries
-func newFingerTable(node *api.Node, m int) fingerTable {
-	ft := make([]*fingerEntry, m)
-	for i := range ft {
-		ft[i] = newFingerEntry(fingerID(node.Id, i, m), node)
+func newFingerTable(nodeStruct *api.Node, m int) fingerTable {
+	ftToBeFilled := make([]*fingerEntry, m)
+	for i := range ftToBeFilled {
+		ftToBeFilled[i] = newFingerEntry(fingerID(nodeStruct.Id, i, m), nodeStruct)
 	}
 
-	return ft
+	return ftToBeFilled
 }
 
 // fingerEntry represents a single finger table entry
 type fingerEntry struct {
-	Id   []byte    // ID hash of (n + 2^i) mod (2^m)
 	Node *api.Node // RemoteNode that Start points to
+	Id   []byte    // ID hash of (n + 2^i) mod (2^m)
 }
 
 // newFingerEntry returns an allocated new finger entry with the attributes set
-func newFingerEntry(id []byte, node *api.Node) *fingerEntry {
+func newFingerEntry(idHash []byte, nodeStruct *api.Node) *fingerEntry {
 	return &fingerEntry{
-		Id:   id,
-		Node: node,
+		Node: nodeStruct,
+		Id:   idHash,
 	}
 }
 
@@ -37,7 +36,7 @@ func newFingerEntry(id []byte, node *api.Node) *fingerEntry {
 func fingerID(n []byte, i int, m int) []byte {
 
 	// Convert the ID to a bigint
-	idInt := (&big.Int{}).SetBytes(n)
+	idBigInt := (&big.Int{}).SetBytes(n)
 
 	// Get the offset
 	two := big.NewInt(2)
@@ -46,40 +45,35 @@ func fingerID(n []byte, i int, m int) []byte {
 
 	// Sum
 	sum := big.Int{}
-	sum.Add(idInt, &offset)
+	sum.Add(idBigInt, &offset)
 
 	// Get the ceiling
 	ceil := big.Int{}
 	ceil.Exp(two, big.NewInt(int64(m)), nil)
 
 	// Apply the mod
-	idInt.Mod(&sum, &ceil)
+	idBigInt.Mod(&sum, &ceil)
 
 	// Add together
-	return idInt.Bytes()
+	return idBigInt.Bytes()
 }
 
 // called periodically. refreshes finger table entries.
 // next stores the index of the next finger to fix.
 func (n *Node) fixFinger(next int) int {
-	nextHash := fingerID(n.Id, next, n.cnf.HashSize)
-	succ, err := n.findSuccessor(nextHash)
 	nextNum := (next + 1) % n.cnf.HashSize
-	if err != nil || succ == nil {
-		fmt.Println("error: ", err, succ)
+	nextHash := fingerID(n.Id, next, n.cnf.HashSize)
+	successor, err := n.findSuccessor(nextHash)
+	if err != nil || successor == nil {
+		fmt.Println("error: ", err, successor)
 		fmt.Printf("finger lookup failed %x %x \n", n.Id, nextHash)
 		// TODO: Check how to handle retry, passing ahead for now
 		return nextNum
 	}
 
-	finger := newFingerEntry(nextHash, succ)
+	finger := newFingerEntry(nextHash, successor)
 	n.ftMtx.Lock()
 	n.fingerTable[next] = finger
-
-	// aInt := (&big.Int{}).SetBytes(nextHash)
-	// bInt := (&big.Int{}).SetBytes(finger.Node.Id)
-	// fmt.Printf("finger entry %d, %d,%d\n", next, aInt, bInt)
-
 	n.ftMtx.Unlock()
 
 	return nextNum
